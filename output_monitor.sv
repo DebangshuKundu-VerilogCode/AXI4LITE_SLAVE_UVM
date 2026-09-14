4a361e4ff44e60218b6d3e9c8b0e7654d996e2fa
DebangshuKundu <debangshurnjkundu@gmail.com>
2026-09-14 22:22:06 +0530

    axi4lite

--- output_monitor.sv
+++ output_monitor.sv
@@ -1,43 +1,77 @@
 class output_monitor extends uvm_monitor;
-        `uvm_component_utils(output_monitor)
-        uvm_analysis_port#(trans) out_monitor_port;
-        virtual axi_if.OUT_MON vif;
-        axi_config m;
-        function new(string name="output_monitor",uvm_component parent);
-                super.new(name,parent);
-        endfunction
-        function void build_phase(uvm_phase phase);
-                super.build_phase(phase);
-        if(!uvm_config_db#(axi_config)::get(this,"","axi_config",m)
-                                        `uvm_fatal(get_type_name(),"output monitor failed")
+    `uvm_component_utils(output_monitor)
+    uvm_analysis_port#(trans) out_monitor_port;
+    virtual axi_if.OUT_MON vif;
+    axi_config m;
+                trans rd_data;
+    function new(string name="output_monitor",uvm_component parent);
+        super.new(name,parent);
+    endfunction
+
+    function void build_phase(uvm_phase phase);
+        super.build_phase(phase);
+        if(!uvm_config_db#(axi_config)::get(this,"","axi_config",m))
+            `uvm_fatal(get_type_name(),"output monitor failed")
         out_monitor_port=new("out_monitor_port",this);
-        endfunction
-        function void connect_phase(uvm_phase phase);
-                super.connect_phase(phase);
-                vif=m.vif;
-        endfunction
-        task run_phase(uvm_phase phase);
-        forever
-                begin
-                        collect_data();
-                end
-        endtask
-        virtual task collect_data();
-        trans rd_data;
-        begin
-        repeat@(vif.out_mon_cb);
-        begin
-        rd_data.AWREADY=vif.out_mon_cb.AWREADY;
-        rd_data.WREADY=vif.out_mon_cb.WREADY;
-        rd_data.BRESP=vif.out_mon_cb.BRESP;
-        rd_data.BVALID=vif.out_mon_cb.BVALID;
-        rd_data.ARREADY=vif.out_mon_cb.ARREADY;
-        rd_data.RDATA=vif.out_mon_cb.RDATA;
-        rd_data.RRESP=vif.out_mon_cb.RRESP;
-        rd_data.RVALID=vif.out_mon_cb.RVALID;
-        end
-        out_monitor_port.write(rd_data);
-        `uvm_info("OUTPUT_MONITOR",$sformatf("OUTPUT MONITOR\n%s",rd_data.sprint()),UVM_NONE)
-        end
-        endtask
-        endclass
+    endfunction
+
+    function void connect_phase(uvm_phase phase);
+        super.connect_phase(phase);
+        vif=m.vif;
+    endfunction
+task run_phase(uvm_phase phase);
+//repeat(2) @(vif.out_mon_cb);
+forever begin
+
+rd_data=trans::type_id::create("rd_data");
+@(vif.out_mon_cb);
+`uvm_info("OUT_DEBUG",
+$sformatf("ARV=%0b ARR=%0b RV=%0b RR=%0b BV=%0b BR=%0b",
+vif.out_mon_cb.ARVALID,
+vif.out_mon_cb.ARREADY,
+vif.out_mon_cb.RVALID,
+vif.out_mon_cb.RREADY,
+vif.out_mon_cb.BVALID,
+vif.out_mon_cb.BREADY),
+UVM_NONE)
+collect_data();
+out_monitor_port.write(rd_data);
+`uvm_info("OUTPUT_MONITOR",$sformatf("OUTPUT MONITOR\n %s",rd_data.sprint()),UVM_NONE)
+end
+endtask
+
+virtual task collect_data();
+forever begin
+@(vif.out_mon_cb);
+if ((vif.out_mon_cb.AWVALID && vif.out_mon_cb.AWREADY) && !(vif.out_mon_cb.WVALID && vif.out_mon_cb.WREADY)) begin
+wait(vif.out_mon_cb.BVALID);
+rd_data.BRESP=vif.out_mon_cb.BRESP;
+rd_data.ARESETn=vif.out_mon_cb.ARESETn;
+return;
+end
+
+else if(!(vif.out_mon_cb.AWVALID && vif.out_mon_cb.AWREADY) && (vif.out_mon_cb.WVALID && vif.out_mon_cb.WREADY)) begin
+rd_data.WDATA=vif.out_mon_cb.WDATA;
+wait(vif.out_mon_cb.BVALID);
+rd_data.BRESP=vif.out_mon_cb.BRESP;
+rd_data.ARESETn=vif.out_mon_cb.ARESETn;
+return;
+end
+
+else if((vif.out_mon_cb.AWVALID && vif.out_mon_cb.AWREADY) && (vif.out_mon_cb.WVALID && vif.out_mon_cb.WREADY)) begin
+wait(vif.out_mon_cb.BVALID);
+rd_data.BRESP=vif.out_mon_cb.BRESP;
+rd_data.ARESETn=vif.out_mon_cb.ARESETn;
+return;
+end
+
+else if(vif.out_mon_cb.ARVALID && vif.out_mon_cb.ARREADY) begin
+wait(vif.out_mon_cb.RVALID);
+rd_data.RRESP=vif.out_mon_cb.RRESP;
+rd_data.RDATA=vif.out_mon_cb.RDATA;
+rd_data.ARESETn=vif.out_mon_cb.ARESETn;
+return;
+end
+end
+endtask
+endclass
